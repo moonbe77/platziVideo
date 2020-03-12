93fd3bcc27bd24e1ef20d192a616aab29e44dcf7
Bernardo <moonbe77@gmail.com>
2020-03-12 13:34:26 +1100

    server build to production

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,21 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+require('dotenv').config();
+
+const isDev = process.env.ENV === 'development';
+const entry = ['./src/frontend/index.js'];
+if (isDev) {
+  entry.push(
+    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true',
+  );
+}
 
 module.exports = {
-  mode: 'development',
-  entry: [
-    './src/frontend/index.js',
-    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true',
-  ],
+  mode: process.env.ENV,
+  entry,
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'src/server/public'),
     filename: 'assets/app.js',
     publicPath: '/',
   },
@@ -61,7 +67,7 @@ module.exports = {
     historyApiFallback: true,
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),    
+    isDev ? new webpack.HotModuleReplacementPlugin() : () => {},
     new MiniCssExtractPlugin({
       filename: 'assets/app.css',
     }),
