4cf058cf76a35be0bcd39cb9353123198e1a3440
Bernardo <moonbe77@gmail.com>
2020-03-12 12:54:52 +1100

    serving SSR main function  preloaded state assets

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { renderRoutes } from 'react-router-config';
+import { StaticRouter } from 'react-router-dom';
+import serverRoutes from '../frontend/routes/serverRoutes';
+import reducer from '../frontend/reducers';
+import initialState from '../frontend/initialState';
 
 dotenv.config();
 const { ENV, PORT } = process.env;
@@ -14,25 +23,46 @@ if (ENV === 'development') {
   const webpackHotMiddleware = require('webpack-hot-middleware');
   const compiler = webpack(wepackConfig);
   const serverConfig = { port: PORT, hot: true };
+
   app.use(webpackDevMiddleware(compiler, serverConfig));
   app.use(webpackHotMiddleware(compiler));
 }
-app.get('*', (req, res) => {
-  res.send(`
+const setResponse = (html, preloadedState) => {
+  return `
     <html>
       <head>
         <title>Platzi Video</title>
         <link rel="stylesheet" href="assets/app.css" type="text/css">
       </head>
       <body>
-        <div id="app"></div>
+        <div id="app">${html}</div>
+          <script>
+          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(
+            /</g,
+            '\\u003c',
+          )}
+        </script>
         <script src="assets/app.js" type="text/javascript"></script>
       </body>
     </html >
-    `);
-});
+    `;
+};
+
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initialState);
+  const preloadedState = store.getState();
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        {renderRoutes(serverRoutes)}
+      </StaticRouter>
+    </Provider>,
+  );
+  res.send(setResponse(html, preloadedState));
+};
+app.get('*', renderApp);
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
-  else console.log('Server running on port:', PORT);
+  else console.log(`Server running on port: http://localhost:${PORT}`);
 });
